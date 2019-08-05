06eb0ab6a8a222a4f68b49f87eb13e5f2f016826
Emily Zhong <ezhong@chanzuckerberg.com>
2019-08-05 14:38:12 -0700

    Initial message sent

--- check_in.py
+++ check_in.py
@@ -1,64 +1,96 @@
 class CheckIn:
-    """Performs the check in and records the state of the individual"""
-
-    WELCOME_BLOCK = {
-        "type": "section",
-        "text": {
-            "type": "mrkdwn",
-            "text": (
-                "Helloooooo! :wave: We're so glad you're here! :blush:\n\n"
-            ),
-        },
-    }
-    DIVIDER_BLOCK = {"type": "divider"}
+    """Performs the check in and records the state of the individual.
+    
+    A check in is composed of multiple questions, each which will (hopefully) garner
+    a response, in the form of a slack reaction.
+    """
 
     def __init__(self, channel):
         self.channel = channel
-        self.username = "ee-bot"
+        self.username = "how you doin'"
         self.icon_emoji = ""
         self.timestamp = ""
-        self.reaction_task_completed = False
-        self.pin_task_completed = False
-
+        self.questions = [{
+            "question": "How are we feelin' emotionally today?",
+            "response_options": [{
+                "title": ":cry:",
+                "description": "I feel terrible today"
+            }, {
+                "title": ":slightly_frowning_face:",
+                "description": "I don't feel great today"
+            }, {
+                "title": ":neutral_face:",
+                "description": "I feel ambivalent today"
+            }, {
+                "title": ":simple_smile:",
+                "description": "I feel good today"
+            }, {
+                "title": ":smile:",
+                "description": "I feel great today"
+            }],
+            "response": "",
+            "ts": ""
+        }, {
+           "question": "How are we feelin' energetically today?",
+           "response_options": [{
+                "title": ":one:",
+                "description": "I feel terrible"
+            }, {
+                "title": ":two:",
+                "description": "I feel amazing"
+            }],
+           "response": "",
+           "ts": ""
+        }]
+        self.current_question = 0
+    
     def get_message_payload(self):
         return {
             "ts": self.timestamp,
             "channel": self.channel,
             "username": self.username,
             "icon_emoji": self.icon_emoji,
-            "blocks": [
-                self.WELCOME_BLOCK,
-                self.DIVIDER_BLOCK,
-                *self._get_check_in_block(),
-            ],
+            "blocks": self._get_question_block(),
         }
 
-    def _get_check_in_block(self):
-        text = (
-            f"How are you doing??\n\n"
-        )
-        information = (
-            f"Stay tuned for more updates :hamster_dance:"
-        )
-        return self._get_task_block(text, information)
+    def _get_question_block(self):
+        blocks = []
+        if self.current_question >= len(self.questions):
+            blocks.append(CheckIn.format_text_block(f"That's all the questions we have for you today! See you next time :blush:\n"))
+        else:
+            if self.current_question == 0:
+                blocks.append(CheckIn.format_text_block(f"Good afternoon! We have {len(self.questions)} questions for you today.\n\n"))
+        
+            current_question = self.questions[self.current_question]
+            text = f"Question number {self.current_question + 1}: {current_question['question']} \n\n" 
+            blocks.append(CheckIn.format_text_block(text))
+            blocks.append(CheckIn.format_question_block(current_question))
+        print(blocks)
+        return blocks
+    
 
-    # TODO: Add functionality for our questions
-    def _get_reaction_block(self):
-        task_checkmark = self._get_checkmark(self.reaction_task_completed)
-        text = (
-            f"{task_checkmark} *Add an emoji reaction to this message* :thinking_face:\n"
-            "You can quickly respond to any message on Slack with an emoji reaction."
-            "Reactions can be used for any purpose: voting, checking off to-do items, showing excitement."
-        )
-        information = (
-            ":information_source: *<https://get.slack.help/hc/en-us/articles/206870317-Emoji-reactions|"
-            "Learn How to Use Emoji Reactions>*"
-        )
-        return self._get_task_block(text, information)
+    # BLOCK FORMATTING METHODS
+    @staticmethod
+    def format_text_block(text):
+        return {"type": "section", "text": {"type": "mrkdwn", "text": text}}
 
     @staticmethod
-    def _get_task_block(text, information):
-        return [
-            {"type": "section", "text": {"type": "mrkdwn", "text": text}},
-            {"type": "context", "elements": [{"type": "mrkdwn", "text": information}]},
-        ]
+    def format_question_block(question_info):
+        buttons = [CheckIn.format_response_option(r) for r in question_info["response_options"]]
+        return {
+            "type": "actions",
+            "elements": buttons
+        }
+       
+    
+    @staticmethod
+    def format_response_option(response_option):
+        return {
+            "type": "button",
+            "text": {
+                "type": "plain_text",
+                "text": f"{response_option['title']} {response_option['description']}",
+                "emoji": True
+            },
+            "value": response_option['title']
+        }
