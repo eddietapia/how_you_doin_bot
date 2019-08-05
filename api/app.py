ec6650128d547d3e3580e78ec870b82374b98955
Eddie Tapia <etapiahe@ucsd.edu>
2019-08-05 14:43:34 -0700

    added api functionality

--- api/app.py
+++ api/app.py
@@ -36,20 +36,23 @@ app.config["DEBUG"] = True
 def home():
     return "<h1>How you doin' slackbot!</h1>"
 
-@app.route('/api/v1/users', methods=['GET'])
+@app.route('/api/v1/users', methods=['GET', 'POST'])
 def api_all():
-    # Check if an ID was provided as part of the URL
-    if 'id' in request.args:
-        id = int(request.args['id'])
-    else:
-        return "Error: No id field provided. Please specify an id."
-    # Create an empty list for our results
-    results = []
-    for user in table:
-        if user['id'] == id:
-            results.append(user)
-    return jsonify(results)
+    if request.method == 'GET':
+        # Check if an ID was provided as part of the URL
+        if 'id' in request.args:
+            id = int(request.args['id'])
+        else:
+            return "Error: No id field provided. Please specify an id."
+        # Create an empty list for our results
+        results = []
+        for user in table:
+            if user['id'] == id:
+                results.append(user)
+        return jsonify(results)
+    elif request.method == 'POST':
+        print('TODO. Send something back')
+        print(request.form)
 
-@app.route('/api/v1/updates', )
 
 app.run()
