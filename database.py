8db1120d8c97d0c6b1223074dd6e29d5969670ec
Eddie Tapia <etapiahe@ucsd.edu>
2019-08-06 00:32:31 -0700

    added connection to heroku database

---
+++ database.py
@@ -0,0 +1,12 @@
+import os
+import psycopg2
+
+DATABASE_URL = os.environ['DATABASE_URL']
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+print("Connected", conn)
+cursor = conn.cursor()
+print(cursor.execute("SELECT version();"))
+
+
+
