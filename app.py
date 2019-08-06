a76280e2f7c54b4c203ef62e1ff1455e5651ac4b
Eddie Tapia <etapiahe@ucsd.edu>
2019-08-05 20:59:14 -0700

    returned success response to slack button

--- app.py
+++ app.py
@@ -54,6 +54,7 @@ def api_all():
     elif request.method == 'POST':
         payload = json.loads(request.form['payload'])
         record = {}
+        return jsonify(success=True)
 
 
 if __name__ == '__main__':
