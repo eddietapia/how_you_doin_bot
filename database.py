7b124300db160db2db5378e805af5c91ca7ceb9a
Eddie Tapia <etapiahe@ucsd.edu>
2019-08-06 00:44:36 -0700

    closed connection when finished executing commands

--- database.py
+++ database.py
@@ -6,50 +6,18 @@ DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 print("Connected", conn)
 cursor = conn.cursor()
-print(cursor.execute("SELECT version();"))
+cursor.execute("SELECT version();")
+record = cursor.fetchone()
+print("You are connected to", record)
 
 
-def create_tables():
-    """ create tables in the PostgreSQL database"""
-    commands = (
-        """
-        CREATE TABLE vendors (
-            vendor_id SERIAL PRIMARY KEY,
-            vendor_name VARCHAR(255) NOT NULL
-        )
-        """,
-        """ CREATE TABLE parts (
-                part_id SERIAL PRIMARY KEY,
-                part_name VARCHAR(255) NOT NULL
-                )
-        """,
-        """
-        CREATE TABLE part_drawings (
-                part_id INTEGER PRIMARY KEY,
-                file_extension VARCHAR(5) NOT NULL,
-                drawing_data BYTEA NOT NULL,
-                FOREIGN KEY (part_id)
-                REFERENCES parts (part_id)
-                ON UPDATE CASCADE ON DELETE CASCADE
-        )
-        """,
-        """
-        CREATE TABLE vendor_parts (
-                vendor_id INTEGER NOT NULL,
-                part_id INTEGER NOT NULL,
-                PRIMARY KEY (vendor_id , part_id),
-                FOREIGN KEY (vendor_id)
-                    REFERENCES vendors (vendor_id)
-                    ON UPDATE CASCADE ON DELETE CASCADE,
-                FOREIGN KEY (part_id)
-                    REFERENCES parts (part_id)
-                    ON UPDATE CASCADE ON DELETE CASCADE
-        )
-        """)
 
-for command in commands:
-    print("EXECUTING")
-    #cursor.execute(command)
+
+# Closing database connection.
+if(conn):
+    cursor.close()
+    conn.close()
+    print("PostgreSQL connection is closed")
 
 
 
