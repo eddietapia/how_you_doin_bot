f50d0e6d9ab792c4e9310c3c8371e8eb12260e12
Eddie Tapia <etapiahe@ucsd.edu>
2019-08-06 18:42:52 -0700

    created function to upload to heroku db

--- update_db.py
+++ update_db.py
@@ -5,36 +5,36 @@ import pandas as pd
 DATABASE_URL = os.environ['DATABASE_URL']
 
 
-# Establish a connection to our heroku database and verify it works
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-cursor = conn.cursor()
-cursor.execute("SELECT version();")
-record = cursor.fetchone()
-print("You are connected to", record)
+def upload_response(response):
+    if not response or len(response) != 5:
+        return 0
 
-# Check that the format was correctly created
-my_table = pd.read_sql('select * from users', conn)
-print("User table\n", my_table)
+    # Establish a connection to our heroku database and verify it works
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    cursor = conn.cursor()
+    cursor.execute("SELECT version();")
+    record = cursor.fetchone()
+    print("You are connected to", record)
 
-# Create the sql query to add a new row into the database
-sql = """INSERT INTO users(user_id, block_id, date, value, feedback)
-        VALUES(%s, %s, %s, %s, %s);"""
-user_id = '2'
-block_id = 'emotion'
-date = '2019-06-05'
-value = 3
-feedback = 'Hello'
-cursor.execute(sql, (user_id, block_id, date, value, feedback))
+    # Create the sql query to add a new row into the database
+    sql = """INSERT INTO users(user_id, block_id, date, value, feedback)
+            VALUES(%s, %s, %s, %s, %s);"""
+    user_id = response[0]
+    block_id = response[1]
+    date = response[2]
+    value = response[3]
+    feedback = response[4]
+    cursor.execute(sql, (user_id, block_id, date, value, feedback))
 
-# Check that the format was correctly created
-my_table = pd.read_sql('select * from users', conn)
-print("User table after pushing new row\n", my_table)
+    # Check that the format was correctly created
+    my_table = pd.read_sql('select * from users', conn)
+    print("User table after pushing new row\n", my_table)
 
-# Save the update to the database
-conn.commit()
+    # Save the update to the database
+    conn.commit()
 
-# Close the database connection
-if conn:
-    cursor.close()
-    conn.close()
-    print("PostgreSQL connection is closed")
+    # Close the database connection
+    if conn:
+        cursor.close()
+        conn.close()
+        print("PostgreSQL connection is closed")
