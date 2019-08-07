84cd819351bf6d841f75d966bcd645929e192952
Emily Zhong <ezhong@chanzuckerberg.com>
2019-08-06 18:07:49 -0700

    asdf

--- app.py
+++ app.py
@@ -122,7 +122,8 @@ def api_all():
                 ]
             }
             response_url = 'https://slack.com/api/dialog.open'
-            response_data = { "trigger_id": payload['trigger_id'], "dialog": dialog,  "token": slack_token }
+            response_data = { "trigger_id": payload['trigger_id'], "dialog": dialog, "token": slack_token }
+            response = requests.post(response_url, json.dumps(response_data), headers=response_headers)
 
         else: # Selecting emotion / energy response
             message_timestamp = float(payload['message']['ts'])
@@ -152,7 +153,7 @@ def api_all():
             selected_text = payload['actions'][0]['text']['text']
             response_data = {'text': f':white_check_mark: Marked your response as {selected_text}. Thanks!\n\n', 'replace_original': True}
 
-        response = requests.post(response_url, json=response_data, headers=response_headers)
+            response = requests.post(response_url, json=response_data, data=response_data, headers=response_headers)
         print(response.text)
         # return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
         return jsonify(success=True)
